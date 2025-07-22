dace6c69bec44df924c968dbfb8bac797d9378d5
hagiwara hibiki <146906515+hibiki128@users.noreply.github.com>
2025-07-22 10:13:27 +0900

    エンジンアップデート 主にクォータニオンに変更

--- Application/Scene/GameScene.cpp
+++ Application/Scene/GameScene.cpp
@@ -10,42 +10,9 @@ void GameScene::Initialize() {
     vp_.Initialize();
     vp_.translation_ = {0.0f, 0.0f, -30.0f};
 
-    /// ===================================================
-    /// 生成
-    /// ===================================================
     debugCamera_ = std::make_unique<DebugCamera>();
-    player_ = std::make_unique<Player>();
-    enemy_ = std::make_unique<Enemy>();
-    followCamera_ = std::make_unique<FollowCamera>();
-    skyDome_ = std::make_unique<SkyDome>();
-    ground_ = std::make_unique<Ground>();
-
-    /// ===================================================
-    /// 初期化
-    /// ===================================================
     debugCamera_->Initialize(&vp_);
-    player_->Init("player");
-    enemy_->Init("enemy");
-    skyDome_->Init("SkyDome");
-    ground_->Init("Ground");
-    followCamera_->Init();
 
-    /// ===================================================
-    /// ポインタ共有
-    /// ===================================================
-    enemy_ptr = enemy_.get();
-    player_ptr = player_.get();
-    MotionEditor::GetInstance()->Register(player_ptr);
-    MotionEditor::GetInstance()->Register(enemy_ptr);
-
-    /// ===================================================
-    /// セット
-    /// ===================================================
-    followCamera_->SetPlayer(player_.get());
-    player_->SetCamera(followCamera_.get());
-    player_->SetEnemy(enemy_.get());
-    BaseObjectManager::GetInstance()->AddObject(std::move(player_));
-    BaseObjectManager::GetInstance()->AddObject(std::move(enemy_));
 }
 
 void GameScene::Finalize() {
@@ -53,19 +20,11 @@ void GameScene::Finalize() {
 }
 
 void GameScene::Update() {
-
-   /* if (!debugCamera_->GetActive()) {
-        BaseObjectManager::GetInstance()->Update();
-    }*/
-
     // カメラ更新
     CameraUpdate();
 
     // シーン切り替え
     ChangeScene();
-
-    skyDome_->Update();
-    ground_->Update();
 }
 
 void GameScene::Draw() {
@@ -80,14 +39,11 @@ void GameScene::Draw() {
     //-------------------------
 
     //-----3DObjectの開始-----
-    skyDome_->Draw(vp_);
-    ground_->Draw(vp_);
-    DrawLine3D::GetInstance()->DrawGrid(-0.95f, 64, 1000, {0.0f, 0.0f, 1.0f, 0.75f});
+ 
     //-----------------------
 
     //------Particleの描画開始-------
-    enemy_ptr->DrawParticle(vp_);
-    player_ptr->DrawParticle(vp_);
+
     //-----------------------------
 
     /// Spriteの描画準備
@@ -121,18 +77,13 @@ void GameScene::DrawForOffScreen() {
 
 void GameScene::AddSceneSetting() {
     debugCamera_->imgui();
-    followCamera_->imgui();
 
     MotionEditor::GetInstance()->DrawImGui();
 
 }
 
 void GameScene::AddObjectSetting() {
-    player_ptr->Debug();
 
-    for (auto& bullet:player_ptr->GetBullets()) {
-        bullet->ImGui();
-    }
 }
 
 void GameScene::AddParticleSetting() {
@@ -142,10 +93,7 @@ void GameScene::CameraUpdate() {
     if (debugCamera_->GetActive()) {
         debugCamera_->Update();
     } else {
-        followCamera_->Update();
-        vp_.matWorld_ = followCamera_->GetViewProjection().matWorld_;
-        vp_.matView_ = followCamera_->GetViewProjection().matView_;
-        vp_.matProjection_ = followCamera_->GetViewProjection().matProjection_;
+        vp_.UpdateMatrix();
     }
 }
 
--- application/GameObject/Player/State/Ground/PlayerStateJump.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../Base/PlayerBaseState.h"
-class PlayerStateJump : public PlayerBaseState {
-  public:
-    PlayerStateJump() = default;
-    void Enter(Player &player) override;
-    void Update(Player &player) override;
-    void Exit(Player &player) override;
-};
