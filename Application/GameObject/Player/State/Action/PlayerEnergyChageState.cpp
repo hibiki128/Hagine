114495f8b96e094016c5514cdb18e704a1828c3c
hagiwara hibiki <146906515+hibiki128@users.noreply.github.com>
2025-12-09 17:04:01 +0900

    チャージステートクラスのみ作成

---
+++ Application/GameObject/Player/State/Action/PlayerEnergyChageState.cpp
@@ -0,0 +1,13 @@
+#include "PlayerEnergyChageState.h"
+
+void PlayerEnergyChageState::Enter(Player &player) {
+
+}
+
+void PlayerEnergyChageState::Update(Player &player) {
+
+}
+
+void PlayerEnergyChageState::Exit(Player &player) {
+
+}
